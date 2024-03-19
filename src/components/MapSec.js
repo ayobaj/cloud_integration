60338cc579c2dba0392e5932e96ed1d89d8f0a89
ayo baj <ayobaj007@gmail.com>
2024-03-19 16:40:21 +0100

    commit

---
+++ src/components/MapSec.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const MapSec = (props) => {
+    return (
+        <Map
+        google={props.google}
+        zoom={14}
+        initialCenter={{
+            lat: 37.774929,
+            lng: -122.419416
+        }}
+        />
+    );
+}
+
+MapSec.propTypes = {
+    google: PropTypes.object.isRequired
+}
+
+export default GoogleApiWrapper({
+    apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+})(MapSec);
