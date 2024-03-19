b4a6711aef6d7170c16a92d135ec2ffb2f601202
ayo baj <ayobaj007@gmail.com>
2024-03-19 23:34:49 +0100

    restructure of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 
-
-const App = () => {
+const  App = () => {
   return (
-    <div>
-      
+    <div className="App">
+      <div id="map">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
